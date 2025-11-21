bb3562ee5c054d423e799580b7dbbd16f669df78
Rémi Benoit-kang <remi.benoit-kang@epitech.eu>
2025-11-21 23:00:46 +0100

    croquis menu fait

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Button, HStack } from "@chakra-ui/react"
-import { Provider } from "@/components/ui/provider"
-import { AbsoluteCenter, Box, For, Text, VStack } from "@chakra-ui/react"
-import { Center, Circle, Square } from "@chakra-ui/react"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
-import "./style.css"
+import { Button, Flex, HStack } from "@chakra-ui/react";
+import { Provider } from "@/components/ui/provider";
+import { AbsoluteCenter, Box, For, Text, VStack } from "@chakra-ui/react";
+import { Center, Circle, Square } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./style.css";
+import { FlakesTexture } from "three/examples/jsm/Addons.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      
-      <Center bg="tomato" h="100px" color="white">
-        <h1>Transformist</h1>
-        <HStack>
-          <Button colorPalette="pink" variant="solid" size="xl">
-              Start the fun!
-          </Button>
-        </HStack>
-      </Center>
+      <Flex justifyContent={"flex-start"}>
+        <Box background="tomato" width="100%" p="7" color="white">
+          This is the Box
+        </Box>
+      </Flex>
+      <Flex justifyContent={"flex-end"} width="100%" >
+        <Box background="grey" width="25%" p="25rem" color="white"></Box>
+      </Flex>
+      <Flex justifyContent={"flex-end"} width="100%" >
+        <Box background="blue" width="100%" p="10rem" color="white"></Box>
+      </Flex>
       <App />
     </Provider>
   </StrictMode>
