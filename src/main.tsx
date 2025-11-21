b0234a2224ac86d8c3ba4fdb4875785e92c4a7f1
Killian <killian.chevalier@epitech.eu>
2025-11-21 21:27:06 +0100

    Added button and title.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Provider } from "@/components/ui/provider"
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <h1>Hello world!</h1>
+      <Button>Click me</Button>
+      <App />
+    </Provider>
   </StrictMode>
 );
