ddc483355c926667ff2e84aad9a0d5260c5d430d
Killian <killian.chevalier@epitech.eu>
2025-11-21 21:53:28 +0100

    Added chakra button and center block

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Button, HStack } from "@chakra-ui/react"
 import { Provider } from "@/components/ui/provider"
+import { AbsoluteCenter, Box, For, Text, VStack } from "@chakra-ui/react"
+import { Center, Circle, Square } from "@chakra-ui/react"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
+import "./style.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <h1>Hello world!</h1>
-      <Button>Click me</Button>
+      
+      <Center bg="tomato" h="100px" color="white">
+        <h1>Transformist</h1>
+        <HStack>
+          <Button colorPalette="pink" variant="solid" size="xl">
+              Start the fun!
+          </Button>
+        </HStack>
+      </Center>
       <App />
     </Provider>
   </StrictMode>
