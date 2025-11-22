0569480f5d9186017663af75428cd979a4a597a9
Rémi Benoit-kang <remi.benoit-kang@epitech.eu>
2025-11-22 15:05:33 +0100

    Misc change

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,47 +1,53 @@
 import { useUser } from "./hooks/useUser.ts";
 import { Flex, Heading } from "@chakra-ui/react";
-import { Grid, Box } from "@chakra-ui/react"
+import { Grid, Box } from "@chakra-ui/react";
 import Grid_items from "./pages/components/grid_item.tsx";
 
 export default function Results() {
-    const user = useUser();
+  const user = useUser();
 
-    return (
-        <Flex p={4} direction={"column"} h={"100vh"}>
-            <Flex
-                bg="grey"
-                w="100%"
-                p="4"
-                justify="space-between"
-                alignItems="center"
-              >
-                <Heading>
-                    That's the end, {user.name}! {" "}
-                </Heading>
-                <Heading marginLeft="auto">
-                    Transformist
-                </Heading>
-            </Flex>  
-            <Box marginLeft={"auto"} marginRight={"auto"} marginTop={"auto"} marginBottom={"auto"}>
-                <Heading marginLeft={"auto"} marginRight={"auto"}>
-                    It's time for the results!
-                </Heading>
-                <Flex direction={"column"} marginLeft={"auto"} marginRight={"auto"} marginTop={"auto"} marginBottom={"auto"}>
-                    <Grid
-                        h="200px"
-                        templateRows="repeat(2, 1fr)"
-                        templateColumns="repeat(5, 1fr)"
-                        gap={4}
-                        >
-                        <Grid_items/>
-                        <Grid_items/>
-                        <Grid_items/>
-                        <Grid_items/>
-                        <Grid_items/>
-                    </Grid>
-                </Flex>
-            </Box>
-  
+  return (
+    <Flex p={4} direction={"column"} h={"100vh"}>
+      <Flex
+        bg="grey"
+        w="100%"
+        p="4"
+        justify="space-between"
+        alignItems="center"
+      >
+        <Heading>That's the end, {user.name}! </Heading>
+        <Heading marginLeft="auto">Transformist</Heading>
+      </Flex>
+      <Box
+        marginLeft={"auto"}
+        marginRight={"auto"}
+        marginTop={"auto"}
+        marginBottom={"auto"}
+      >
+        <Heading marginLeft={"auto"} marginRight={"auto"}>
+          It's time for the results!
+        </Heading>
+        <Flex
+          flexDirection={"column"}
+          marginLeft={"auto"}
+          marginRight={"auto"}
+          marginTop={"auto"}
+          marginBottom={"auto"}
+        >
+          <Grid
+            h="200px"
+            templateRows="repeat(2, 1fr)"
+            templateColumns="repeat(5, 1fr)"
+            gap={4}
+          >
+            <Grid_items />
+            <Grid_items />
+            <Grid_items />
+            <Grid_items />
+            <Grid_items />
+          </Grid>
+        </Flex>
+      </Box>
     </Flex>
-    );
+  );
 }
--- src/pages/components/grid_item.tsx
+++ src/pages/components/grid_item.tsx
@@ -1,20 +1,27 @@
-import { GridItem, Box } from "@chakra-ui/react"
+import { GridItem, Box } from "@chakra-ui/react";
 import { useUser } from "../../hooks/useUser";
-import { RatingGroup } from "@chakra-ui/react"
+import { RatingGroup } from "@chakra-ui/react";
 
 export default function Grid_items() {
-    const user = useUser();
-    const user2 = "Killian"
+  const user = useUser();
+  const user2 = "Killian";
+  const score = 3;
 
-    return (
+  return (
     <GridItem rowSpan={1.2}>
-        <Box bg="tomato" h="100%" w="100%" p="4" color="white" borderRadius="l3">
-            {user2}'s final grade : {" "}
-            <RatingGroup.Root readOnly count={5} defaultValue={3} size="sm" color="pink">
-                <RatingGroup.HiddenInput />
-                <RatingGroup.Control />
-            </RatingGroup.Root>
-        </Box>
+      <Box bg="tomato" h="100%" w="100%" p="4" color="white" borderRadius="l3">
+        {user2}'s final grade :{" "}
+        <RatingGroup.Root
+          readOnly
+          count={5}
+          defaultValue={score}
+          size="sm"
+          color="pink"
+        >
+          <RatingGroup.HiddenInput />
+          <RatingGroup.Control />
+        </RatingGroup.Root>
+      </Box>
     </GridItem>
-    );
+  );
 }
